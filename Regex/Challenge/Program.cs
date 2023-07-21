6bc92b47c767675b7f2b21e820c361220146eb73
virajSandakelum <2020cs011@stu.ucsc.cmb.ac.lk>
2023-07-21 17:52:56 +0530

    Regular Expressions challenge

---
+++ Regex/Challenge/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.RegularExpressions;
+
+static string ReverseDateFormat(string sourceDate){
+    const int TIMEOUT = 1000;
+
+    try{
+        return Regex.Replace(sourceDate, @"^(?<mon>\d{1,2})/(?<day>\d{1,2})/(?<year>\d{1,2})$"),"${year}-${mon}-${day}",RegexOptions.None);
+    }catch(RegexMatchTimeoutException){
+        return sourceDate;
+    }
+}
+
+
+do{
+    Console.WriteLine("Date to convert? (Use mm/dd/yyyy, or 'ext' to quit)");
+
+    string inputStr = Console.ReadLine();
+
+    if(inputStr == 'exit'){
+        break;
+    }
+
+    DateTime result;
+
+    if(DateTime.TryParse(inputStr, out result)){
+        string reverseDate = ReverseDateFormat(inputStr);
+        Console.WriteLine($"The reversed format is {reverseDate}");
+    }else{
+        Console.WriteLine("That's not a valid date,try again");
+    }
+}while(true);
